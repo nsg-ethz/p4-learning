aac81e24e19d001ece8b2496b17d7d073ef1656c
jurijnota <jurij.nota@outlook.com>
2021-03-28 10:58:21 +0000

    l2_learning exercise update

--- exercises/04-L2_Learning/l2_learning_controller.py
+++ exercises/04-L2_Learning/l2_learning_controller.py
@@ -1,39 +1,36 @@
 import nnpy
 import struct
-from p4utils.utils.topology import Topology
+from p4utils.utils.helper import load_topo
 from p4utils.utils.sswitch_API import SimpleSwitchAPI
 from scapy.all import Ether, sniff, Packet, BitField, raw
+
 
 class CpuHeader(Packet):
     name = 'CpuPacket'
     fields_desc = [BitField('macAddr',0,48), BitField('ingress_port', 0, 16)]
 
+
 class L2Controller(object):
 
     def __init__(self, sw_name):
-
-        self.topo = Topology(db="topology.db")
+        self.topo = load_topo('topology.json')
         self.sw_name = sw_name
         self.thrift_port = self.topo.get_thrift_port(sw_name)
         self.cpu_port =  self.topo.get_cpu_port_index(self.sw_name)
         self.controller = SimpleSwitchAPI(self.thrift_port)
-
         self.init()
 
     def init(self):
-
         self.controller.reset_state()
         self.add_boadcast_groups()
         self.add_mirror()
 
     def add_mirror(self):
-
         if self.cpu_port:
             self.controller.mirroring_add(100, self.cpu_port)
 
     def add_boadcast_groups(self):
-
-        interfaces_to_port = self.topo[self.sw_name]["interfaces_to_port"].copy()
+        interfaces_to_port = self.topo.NodeIntfs(fields=['port'])[self.sw_name]
         #filter lo and cpu port
         interfaces_to_port.pop('lo', None)
         interfaces_to_port.pop(self.topo.get_cpu_port_intf(self.sw_name), None)
@@ -41,22 +38,16 @@ class L2Controller(object):
         mc_grp_id = 1
         rid = 0
         for ingress_port in interfaces_to_port.values():
-
             port_list = list(interfaces_to_port.values())
             del(port_list[port_list.index(ingress_port)])
-
             #add multicast group
             self.controller.mc_mgrp_create(mc_grp_id)
-
             #add multicast node group
             handle = self.controller.mc_node_create(rid, port_list)
-
             #associate with mc grp
             self.controller.mc_node_associate(mc_grp_id, handle)
-
             #fill broadcast table
             self.controller.table_add("broadcast", "set_mcast_grp", [str(ingress_port)], [str(mc_grp_id)])
-
             mc_grp_id +=1
             rid +=1
 
--- exercises/04-L2_Learning/solution/l2_learning_controller.py
+++ exercises/04-L2_Learning/solution/l2_learning_controller.py
@@ -1,6 +1,5 @@
 import nnpy
 import struct
-from p4utils.utils.topology import NetworkGraph
 from p4utils.utils.helper import load_topo
 from p4utils.utils.sswitch_API import SimpleSwitchAPI
 from scapy.all import Ether, sniff, Packet, BitField, raw
@@ -32,7 +31,6 @@ class L2Controller(object):
             self.controller.mirroring_add(100, self.cpu_port)
 
     def add_boadcast_groups(self):
-
         interfaces_to_port = self.topo.NodeIntfs(fields=['port'])[self.sw_name]
         #filter lo and cpu port
         interfaces_to_port.pop('lo', None)
@@ -41,22 +39,16 @@ class L2Controller(object):
         mc_grp_id = 1
         rid = 0
         for ingress_port in interfaces_to_port.values():
-
             port_list = list(interfaces_to_port.values())
             del(port_list[port_list.index(ingress_port)])
-
             #add multicast group
             self.controller.mc_mgrp_create(mc_grp_id)
-
             #add multicast node group
             handle = self.controller.mc_node_create(rid, port_list)
-
             #associate with mc grp
             self.controller.mc_node_associate(mc_grp_id, handle)
-
             #fill broadcast table
             self.controller.table_add("broadcast", "set_mcast_grp", [str(ingress_port)], [str(mc_grp_id)])
-
             mc_grp_id +=1
             rid +=1
 
