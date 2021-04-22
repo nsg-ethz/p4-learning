90a3da17cb977c09e968c47861291f790c4810ca
jurijnota <jurij.nota@outlook.com>
2021-04-22 10:22:50 +0000

    small change

--- exercises/03-L2_Basic_forwarding/p4runtime/solution/controller.py
+++ exercises/03-L2_Basic_forwarding/p4runtime/solution/controller.py
@@ -9,8 +9,8 @@ controllers = {}
 
 for switch, data in topo.get_p4rtswitches().items():
     controllers[switch] = SimpleSwitchP4RuntimeAPI(data['device_id'], data['grpc_port'],
-                                                  p4rt_path=data['p4rt_path'],
-                                                  json_path=data['json_path'])
+                                                   p4rt_path=data['p4rt_path'],
+                                                   json_path=data['json_path'])
 
 controller = controllers['s1']                        
 
