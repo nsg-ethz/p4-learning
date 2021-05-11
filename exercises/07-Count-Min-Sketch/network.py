22719c80a04d5c6101ea95e39ea12d2215a671f4
jurijnota <jurij.nota@outlook.com>
2021-05-11 13:14:17 +0000

    07-Count-Min-Sketch updated with NetworkAPI

---
+++ exercises/07-Count-Min-Sketch/network.py
@@ -0,0 +1,25 @@
+from p4utils.utils.network_API import NetworkAPI
+
+net = NetworkAPI()
+
+# Network general options
+net.setLogLevel('info')
+
+# Network definition
+net.addP4Switch('s1', conf_path='s1-commands.txt')
+net.setP4SourceAll('p4src/cm-sketch.p4')
+
+net.addHost('h1')
+net.addHost('h2')
+
+net.addLink("h1", "s1")
+net.addLink("h2", "s1")
+
+# Assignment strategy
+net.mixed()
+
+# Nodes general options
+net.disablePcapDumpAll()
+net.disableLogAll()
+net.enableCli()
+net.startNetwork()
