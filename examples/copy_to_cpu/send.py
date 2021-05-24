0f9168312f0dcb87a9534ac27cf981ea27bddd26
jurijnota <jurij.nota@outlook.com>
2021-05-24 12:45:23 +0000

    copy_to_cpu updated

--- examples/copy_to_cpu/send.py
+++ examples/copy_to_cpu/send.py
@@ -15,22 +15,22 @@ def get_if():
     for i in get_if_list():
         if "eth0" in i:
             iface=i
-            break;
+            break
     if not iface:
-        print "Cannot find eth0 interface"
+        print("Cannot find eth0 interface")
         exit(1)
     return iface
 
 def main():
 
-    if len(sys.argv)<3:
-        print 'pass 2 arguments: <destination> "<message>"'
+    if len(sys.argv)<2:
+        print('pass 2 arguments: <destination>')
         exit(1)
 
     addr = socket.gethostbyname(sys.argv[1])
     iface = get_if()
 
-    print "sending on interface %s to %s" % (iface, str(addr))
+    print("sending on interface %s to %s" % (iface, str(addr)))
     pkt =  Ether(src=get_if_hwaddr(iface),dst = "00:00:00:00:01:12")
     pkt = pkt /IP(dst=addr, tos=1)
     pkt.show2()
