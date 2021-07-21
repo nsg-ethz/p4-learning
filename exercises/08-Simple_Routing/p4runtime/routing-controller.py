d9f8dc63fa4bc831fcd8784bfdd176a7d26fa740
Jurij Nota <jurij.nota@outlook.com>
2021-07-21 11:39:03 +0200

    fix indentation

--- exercises/08-Simple_Routing/p4runtime/routing-controller.py
+++ exercises/08-Simple_Routing/p4runtime/routing-controller.py
@@ -3,7 +3,7 @@ from p4utils.utils.sswitch_thrift_API import SimpleSwitchThriftAPI
 
 class RoutingController(object):
 
-def __init__(self):
+    def __init__(self):
 
         if not os.path.exists('topology.json'):
             print('Could not find topology object!!!\n')
@@ -13,44 +13,44 @@ def __init__(self):
         self.controllers = {}
         self.init()
 
-    def init(self):
-        self.connect_to_switches()
-        self.reset_states()
-        self.set_table_defaults()
+        def init(self):
+            self.connect_to_switches()
+            self.reset_states()
+            self.set_table_defaults()
 
-    def reset_states(self):
-        """Resets registers, tables, etc.
-        """
-        for p4rtswitch, controller in self.controllers.items():
-            # Reset grpc server
-            controller.reset_state()
+        def reset_states(self):
+            """Resets registers, tables, etc.
+            """
+            for p4rtswitch, controller in self.controllers.items():
+                # Reset grpc server
+                controller.reset_state()
 
-            # Connect to thrift server
-            thrift_port = self.topo.get_thrift_port(p4rtswitch)
-            controller_thrift = SimpleSwitchThriftAPI(thrift_port)
-            # Reset forwarding states
-            controller_thrift.reset_state()
+                # Connect to thrift server
+                thrift_port = self.topo.get_thrift_port(p4rtswitch)
+                controller_thrift = SimpleSwitchThriftAPI(thrift_port)
+                # Reset forwarding states
+                controller_thrift.reset_state()
 
-    def connect_to_switches(self):
-        for p4rtswitch, data in self.topo.get_p4switches().items():
-            device_id = self.topo.get_p4switch_id(p4rtswitch)
-            grpc_port = self.topo.get_grpc_port(p4rtswitch)
-            p4rt_path = data['p4rt_path']
-            json_path = data['json_path']
-            self.controllers[p4rtswitch] = SimpleSwitchP4RuntimeAPI(device_id, grpc_port,
-                                                                    p4rt_path=p4rt_path,
-                                                                    json_path=json_path)
+        def connect_to_switches(self):
+            for p4rtswitch, data in self.topo.get_p4switches().items():
+                device_id = self.topo.get_p4switch_id(p4rtswitch)
+                grpc_port = self.topo.get_grpc_port(p4rtswitch)
+                p4rt_path = data['p4rt_path']
+                json_path = data['json_path']
+                self.controllers[p4rtswitch] = SimpleSwitchP4RuntimeAPI(device_id, grpc_port,
+                                                                        p4rt_path=p4rt_path,
+                                                                        json_path=json_path)
 
-    def set_table_defaults(self):
-        for controller in self.controllers.values():
-            controller.table_set_default("ipv4_lpm", "drop", [])
-            controller.table_set_default("ecmp_group_to_nhop", "drop", [])
+        def set_table_defaults(self):
+            for controller in self.controllers.values():
+                controller.table_set_default("ipv4_lpm", "drop", [])
+                controller.table_set_default("ecmp_group_to_nhop", "drop", [])
 
-    def route(self):
-        """implement this function"""
+        def route(self):
+            """implement this function"""
 
-    def main(self):
-        self.route()
+        def main(self):
+            self.route()
 
 
 if __name__ == "__main__":
