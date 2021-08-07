f070a8d18fd42d2f865346fd25ac9c3ac97ee934
jurijnota <jurij.nota@outlook.com>
2021-08-07 15:29:01 +0000

    Topo image added

--- examples/frr_manual_setup/run_topo.sh
+++ examples/frr_manual_setup/run_topo.sh
@@ -73,13 +73,13 @@ sudo ip netns exec r3 ip link set R3_port_R2 mtu 9500
 sudo ip netns exec r3 sysctl net.ipv6.conf.R3_port_R2.disable_ipv6=1
 
 # Run FRRouters
-sudo ip netns exec r1 zebra -d -u root -g root -N r1 -M fpm -i /tmp/r1-zebra.pid --log file:/tmp/r1-zebra.log --log-level debugging > /tmp/r1-zebra.out 2>&1
+sudo ip netns exec r1 zebra -d -u root -g root -N r1 -i /tmp/r1-zebra.pid --log file:/tmp/r1-zebra.log --log-level debugging > /tmp/r1-zebra.out 2>&1
 sudo ip netns exec r1 ospfd -d -u root -g root -N r1 -i /tmp/r1-ospfd.pid --log file:/tmp/r1-ospfd.log --log-level debugging > /tmp/r1-ospfd.out 2>&1
 
-sudo ip netns exec r2 zebra -d -u root -g root -N r2 -M fpm -i /tmp/r2-zebra.pid --log file:/tmp/r2-zebra.log --log-level debugging > /tmp/r2-zebra.out 2>&1
+sudo ip netns exec r2 zebra -d -u root -g root -N r2 -i /tmp/r2-zebra.pid --log file:/tmp/r2-zebra.log --log-level debugging > /tmp/r2-zebra.out 2>&1
 sudo ip netns exec r2 ospfd -d -u root -g root -N r2 -i /tmp/r2-ospfd.pid --log file:/tmp/r2-ospfd.log --log-level debugging > /tmp/r2-ospfd.out 2>&1
 
-sudo ip netns exec r3 zebra -d -u root -g root -N r3 -M fpm -i /tmp/r3-zebra.pid --log file:/tmp/r3-zebra.log --log-level debugging > /tmp/r3-zebra.out 2>&1
+sudo ip netns exec r3 zebra -d -u root -g root -N r3 -i /tmp/r3-zebra.pid --log file:/tmp/r3-zebra.log --log-level debugging > /tmp/r3-zebra.out 2>&1
 sudo ip netns exec r3 ospfd -d -u root -g root -N r3 -i /tmp/r3-ospfd.pid --log file:/tmp/r3-ospfd.log --log-level debugging > /tmp/r3-ospfd.out 2>&1
 
 sudo vtysh -N r1 -f r1.conf
@@ -115,3 +115,6 @@ sudo ip netns exec r2 tcpdump -i R2_port_R3 -Q out --packet-buffered -w ./pcap/R
 
 sudo ip netns exec r3 tcpdump -i R3_port_R2 -Q in --packet-buffered -w ./pcap/R3_port_R2_in.pcap &
 sudo ip netns exec r3 tcpdump -i R3_port_R2 -Q out --packet-buffered -w ./pcap/R3_port_R2_out.pcap &
+
+# Check OSPF convergence time using ping
+sudo ip netns exec h1 ping 1.0.3.2
